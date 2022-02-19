8e2b4434b19486b126e910186555dc9bf981e94b
Raxesh Shah <raxesh35@gmail.com>
2022-02-19 17:45:29 +0530

    Craete user,note schema,models, added validation and save user data in to mongo db

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,13 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+// All Routes
+
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ backend/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
+
+router.post('/',[
+    body('name', 'Please enter a valid name').isLength({ min: 3 }),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Password must have minimum 5 characters').isLength({ min: 5 }),
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err=>{console.log(err)
+    res.json({error: 'Please enter a unique email id'})});
+})
+
+module.exports = router;
