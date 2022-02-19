fe9c8bf469c75082aa0071d44b7c1218962e1a5e
Raxesh Shah <raxesh35@gmail.com>
2022-02-19 19:18:08 +0530

    Added bcrypt,jwt package for hash password and create auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,25 +2,45 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.post('/',[
+const JWT_TOKEN = "raxkareactmind";
+router.post('/', [
     body('name', 'Please enter a valid name').isLength({ min: 3 }),
     body('email', 'Please enter a valid email').isEmail(),
     body('password', 'Password must have minimum 5 characters').isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
-    
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-    res.json({error: 'Please enter a unique email id'})});
-})
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ errors: "Sorry email is already exists" });
+        }
+        const salt = await bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
 
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        })
+        const data = {
+            user : {
+                id:user.id
+            }
+        };
+        const authToken = jwt.sign(data, JWT_TOKEN);
+        res.json({authToken})
+        //res.json(user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some errors occured (" + error.message + ")");
+    }
+
+})
 module.exports = router;
